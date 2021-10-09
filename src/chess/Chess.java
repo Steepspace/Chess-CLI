b82674bb92093dd7f871a94b9760e3c8259ffd61
Apurva Narde <apurva.narde@rutgers.edu>
2021-10-09 14:19:06 -0400

    Main Chess
    - added imports for classes in piece and board
    - in main added test code for toString in Board.java

---
+++ src/chess/Chess.java
@@ -0,0 +1,12 @@
+package chess;
+
+import chess.piece.*;
+import chess.board.*;
+
+public class Chess {
+    public static void main(String[] args) {
+        // Testing toString in board
+        // Board board = new Board();
+        // System.out.print(board);
+    }
+}
